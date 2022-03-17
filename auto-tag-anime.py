9555fef667263d2404d52fcc27e4da9aedafa2d6
Joe_Siu <joesiu.dev@gmail.com>
2022-03-17 13:25:16 +0800

    Fixed non jpg/jpeg error not shown

--- auto-tag-anime.py
+++ auto-tag-anime.py
@@ -23,7 +23,8 @@ class addAnimeTags():
         if sys.platform == 'win32':   
             file_ext = os.path.splitext(path)[1]
             if file_ext != '.jpg' and file_ext != '.jpeg':
-                return path + " is not a JPEG, no exif data"
+                print(path + " is not a JPEG, no exif data")
+                return
         
         status, tags = self.model.classify_image(path)
         if status == 'success':
